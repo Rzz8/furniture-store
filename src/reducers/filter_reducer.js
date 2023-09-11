04a45b97f661955ea9229795d1fe64c36b860209
R Zhong <30917072+Rzz8@users.noreply.github.com>
2023-09-11 22:29:47 +1000

    update filter default values and set max price and price after fetching products

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -9,12 +9,17 @@ import {
   CLEAR_FILTERS,
 } from "../actions";
 
+//
 const filter_reducer = (state, action) => {
+  //Math.max() can not process an array and we should use the spread operator to copy the array
   if (action.type === LOAD_PRODUCTS) {
+    let maxPrice = action.payload.map((product) => product.price);
+    maxPrice = Math.max(...maxPrice);
     return {
       ...state,
       all_products: [...action.payload],
       filtered_products: [...action.payload],
+      filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
     };
   }
 
@@ -30,6 +35,7 @@ const filter_reducer = (state, action) => {
     return { ...state, sort: action.payload };
   }
 
+  // in reducer function we don't want to modify the old state to the new state, so use spread operator
   if (action.type === SORT_PRODUCTS) {
     const { filtered_products } = state;
     let temp_products = [...filtered_products];
